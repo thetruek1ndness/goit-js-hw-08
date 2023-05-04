716b89f24b2ac5fc7126871f6cb912952faedfcc
thetruek1ndness <andrii.ptrnk@gmail.com>
2023-05-05 00:31:18 +0300

    tast 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,8 @@ const iframeRef = document.querySelector('iframe');
 const player = new Player(iframeRef);
 
 if (localStorage.getItem('videoplayer-current-time')) {
-  player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
   );
   localStorage.removeItem('videoplayer-current-time');
 }
@@ -15,8 +16,3 @@ player.on('timeupdate', throttle(playerHandler, 1000));
 function playerHandler(evt) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
 }
-
-
-
-
-
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import trottle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+const userData = {};
+
+formRef.addEventListener('input', trottle(userTypingHandler, 500));
+formRef.addEventListener('submit', submitBtnHandler);
+
+if (JSON.parse(localStorage.getItem(STORAGE_KEY))) {
+  const { email, message } = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  formRef.email.value = email;
+  formRef.message.value = message;
+}
+
+function userTypingHandler(evt) {
+  userData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+function submitBtnHandler(evt) {
+  evt.preventDefault();
+  
+  if(!(formRef.email.value && formRef.message.value)){
+    console.log("Заповніть всі поля");
+    return;
+  }
+
+  console.log(userData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  return;
+}
