4f440f472aada6fa221a224327c5a48463416f03
thetruek1ndness <andrii.ptrnk@gmail.com>
2023-05-04 17:20:25 +0300

    shos

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import SimpleLightbox from "simplelightbox";
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) =>
+  `<li class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src = '${preview}' alt='${description}' title='${description}' />
+    </a>
+  </li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+    
+new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+    });
