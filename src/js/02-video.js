108d7b8562bc392de00d4b2ec96354234d5d9038
thetruek1ndness <andrii.ptrnk@gmail.com>
2023-05-04 23:30:47 +0300

    tast-2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  );
+  localStorage.removeItem('videoplayer-current-time');
+}
+
+player.on('timeupdate', throttle(playerHandler, 1000));
+
+function playerHandler(evt) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
+}
+
+
+
+
 
